e3aa469ddd4f958e93e0aeee755a448b35eb0a92
espoupou <126259211+espoupou@users.noreply.github.com>
2026-03-23 11:43:07 +0100

    Remove pressure loss calculation function
    Removed the calculation function for pressure losses from the main file.

--- main.m
+++ main.m
@@ -153,31 +153,3 @@ xlabel('Diametre (mm)');
 ylabel('Puissance electrique (W)');
 title('Optimisation : P_{elec} en fonction du diametre');
 legend('P_{elec}', 'Seuil 100 W', 'Diametres rejetes', 'Location', 'best');
-
-%% Fonction de calcul des pertes de charge
-function [DP_tot, v] = calculPertes(qv, D, L, eps, Ks, rho, mu, DP_emetteur)
-% qv en m3/s
-% D en m
-% retourne la perte de charge totale en Pa et la vitesse en m/s
-
-A = pi*D^2/4;
-v = qv / A;
-
-if qv == 0
-    DP_tot = DP_emetteur;
-    return;
-end
-
-Re = rho*v*D/mu;
-
-if Re < 2300
-    lambda = 64/Re;
-else
-    lambda = 1 / (-1.8*log10(((eps/D)/3.7)^1.11 + 6.9/Re))^2;
-end
-
-DP_L = lambda*(L/D)*(rho*v^2/2);
-DP_S = Ks*(rho*v^2/2);
-
-DP_tot = DP_L + DP_S + DP_emetteur;
-end
